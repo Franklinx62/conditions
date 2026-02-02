20f82027b732fe276cfe14e86ddb29393ad117b6
Franklinx62 <ahmedmhmoad888@gmail.com>
2026-02-02 08:29:01 +0200

    Refactor number checking logic in condition.c
    there are many error

--- condition.c
+++ condition.c
@@ -3,10 +3,17 @@ int main() {
     int number;
     printf("enter the number:");
     scanf("%d",&number);
-    if (number>0 && number % 2 ==0){
-        printf("this number even\n");
-    }else  (number >0 && number % 2==3){
-        printf("this number oden\n");
+    if (number>0) {
+        if (number % 2 ==0){
+            printf("positive and even\n");
+        }else{
+            printf("postive and odd\n");
+        }
+    else if(number <0){
+        printf("negative number\n");
+        }
+    else{
+        printf("zero?");
     }
     return 0;
     
